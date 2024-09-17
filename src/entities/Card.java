56fda9d5bb4c0e292be3d919346ff68893821455
BroVlad <107715706+BroVlad@users.noreply.github.com>
2024-09-17 13:15:57 -0300

    atualizações

--- src/entities/Card.java
+++ src/entities/Card.java
@@ -5,38 +5,35 @@ public class Card {
 	private String name;
 	private Integer life = 100;
 	private Double power;
-	
+
 	public Card() {
 	}
-	
-	public Card (String name) {
+
+	public Card(String name) {
 		setName(name);
 	}
-	
+
 	public void setName(String newName) {
 		this.name = newName;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setLife(int newDamage) {
 		this.life -= newDamage;
 	}
-	
+
 	public Integer getLife() {
 		return life;
 	}
-	
+
 	public void setPower(Double newPower) {
 		this.power = (newPower / 100) * newPower;
 	}
-	
+
 	public Double getPower() {
 		return power;
 	}
-	
-	
-	
 }
--- src/entities/Skills.java
+++ src/entities/Skills.java
@@ -4,12 +4,29 @@ public class Skills {
 
 	Card card = new Card();
 	
-	public void getShield () {
+	public Skills(Integer randomSelectSkills) {
+		if (randomSelectSkills == 0) {
+			shield();
+		} else if (randomSelectSkills == 1) {
+			cure();
+		} else if(randomSelectSkills == 2) {
+			skipTurn();
+		}
+	}
+	
+	// 1
+	public void shield () {
 		card.setLife(200);
 	}
 	
-	public void getCure () {
+	//2
+	public void cure () {
 		card.setLife(10);
+	}
+	
+	//3
+	public Boolean skipTurn () {
+		return false;
 	}
 	
 }
